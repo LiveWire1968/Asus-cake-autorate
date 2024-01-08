960eacbf2e56f1028359b7f6918736631652dcfc
LiveWire1968 <76123941+LiveWire1968@users.noreply.github.com>
2024-01-08 12:01:40 -0500

    Create launcher.sh

---
+++ launcher.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+cake_instances=(/jffs/scripts/config.*.sh)
+cake_instance_pids=()
+
+trap kill_cake_instances INT TERM EXIT
+
+kill_cake_instances()
+{
+        trap - INT TERM EXIT
+
+        echo "Killing all instances of cake one-by-one now."
+
+        for ((cake_instance=0; cake_instance<${#cake_instances[@]}; cake_instance++))
+        do
+                kill "${cake_instance_pids[${cake_instance}]}" 2>/dev/null || true
+        done
+        wait
+}
+
+for cake_instance in "${cake_instances[@]}"
+do
+        /jffs/scripts/cake-autorate.sh "${cake_instance}" &
+        cake_instance_pids+=(${!})
+done
+wait
